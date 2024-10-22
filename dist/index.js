8f9d801b55df3545b047b3fc8489ef176912256b
revathir-29 <revathirevathi27806@gmail.com>
2024-10-22 15:48:25 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+var num =10;
+function example(args=1){
+    if(args ==1 ){
+        return true;
+
+    }else if(args ==2){
+        return false;
+
+    }else if(args==1){
+        return true;
+        
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+var num =10;
+function example(args=1){
+    if(args ==1 ){
+
+    }else if(args ==2){
+
+    }else if(args==1){
+        
+    }
+}
